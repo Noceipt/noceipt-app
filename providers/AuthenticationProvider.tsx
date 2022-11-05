bd066b42633dcfb1620f4fb91c705be89b04dc97
Cody Molisee <codymolisee@Codys-MacBook-Pro.local>
2022-11-05 14:01:52 -0700

    8: created auth provider

---
+++ providers/AuthenticationProvider.tsx
@@ -0,0 +1,25 @@
+import { createContext, PropsWithChildren, useContext, useState } from 'react';
+
+interface IAuthenticationContext {
+    token: string;
+    setToken: (prevState: string) => void;
+};
+
+const AuthenticationContext = createContext<IAuthenticationContext>({
+    token: '',
+    setToken: () => {},
+});
+
+export const AuthenticationProvider = (
+    props: PropsWithChildren<IAuthenticationContext>
+) => {
+    const [token, setToken] = useState<string>('');
+
+    return (
+        <AuthenticationContext.Provider value={{token, setToken}}>
+            {props.children}
+        </AuthenticationContext.Provider>
+    );
+};
+
+export const useAuthenticationContext = useContext(AuthenticationContext);
