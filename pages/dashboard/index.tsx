595b8a2fbcae85d267dc9e68576168a983d05b21
Christopher Jones <tkcwebdev@gmail.com>
2022-11-05 16:20:57 -0700

    36: Create dashboard page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Page from "../../templates/Page";
+import styles from "../../styles/Home.module.css";
+
+const Dashboard = () => {
+	return (
+		<Page>
+			<h1 className={styles.Dashboard}>Dashboard</h1>
+		</Page>
+	);
+};
+
+export default Dashboard;
