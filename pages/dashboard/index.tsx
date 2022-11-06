5d48366c987159b2ac92182ead94871f40e03032
Cody Molisee <codymolisee@Codys-MacBook-Pro.local>
2022-11-05 17:40:25 -0700

    added search for real

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,10 +1,18 @@
 import Page from "../../templates/Page";
 import styles from "../../styles/Dashboard.module.scss";
+import Search from "../../components/Search";
 
 const Dashboard = () => {
+
+	const searchFilterCallback = (searchInput: string) => {
+		// todo: filter receipts by text search
+		// use provider for receipts
+	}
+
 	return (
 		<Page>
 			<h1 className={styles.Dashboard}>Dashboard</h1>
+			<Search callback={searchFilterCallback}/>
 		</Page>
 	);
 };
