ebd5fd919e7b9467427c202fe7acc821248a59a9
Cody Molisee <codymolisee@Codys-MacBook-Pro.local>
2022-11-05 17:23:29 -0700

    finished

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -2,6 +2,7 @@ import styles from './Search.module.scss';
 import { Box, Button, Input } from '@chakra-ui/react';
 import { FormEvent, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
+import { FiSearch, FiX } from "react-icons/fi";
 
 interface ISearchProps {
     classname?: string;
@@ -33,9 +34,8 @@ const Search = ({
         <Box className={styles.searchContainer} display={'flex'} justifyContent={'flex-end'}>
             <AnimatePresence>
                 {isActive && (
-                    <motion.div initial={{width: 0, opacity: 0}} animate={{width: '100%', opacity: 1}} exit={{ width: 0, opacity: 0 }} transition={{ duration: 0.8 }}>
-                        <Input className={styles.searchInput}
-                            placeholder='Search' 
+                    <motion.div initial={{ width: 0, opacity: 0 }} animate={{ width: '100%', opacity: 1 }} exit={{ width: 0, opacity: 0 }} transition={{ duration: 0.8 }}>
+                        <Input placeholder='Search' 
                             size='md' 
                             type='search' 
                             onChange={handleChange} 
@@ -43,21 +43,25 @@ const Search = ({
                     </motion.div>
                 )}
             </AnimatePresence>
-            {!isActive ? (
-                <Button className={styles.searchBtn} onClick={handleClick}>
-                    openSearch
-                </Button>
-            ) : (
-                <>
-                    <Button className={styles.searchBtn} onClick={handleSubmit}>
-                        search
-                    </Button>
+            <AnimatePresence>
+                {!isActive ? (
                     <Button className={styles.searchBtn} onClick={handleClick}>
-                        closeSearch
+                    <FiSearch size={22}/>
+                </Button>
+                ) : (
+                    <>
+                    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.8 }}>
+                        <Button className={styles.searchBtn} onClick={handleClick}>
+                            <FiX  size={22}/>
+                        </Button>
+                    </motion.div>
+                    <Button className={styles.searchBtn} onClick={handleSubmit}>
+                        <FiSearch size={22} />
                     </Button>
                 </>
-            )}
-    </Box>
+                )}
+            </AnimatePresence>
+        </Box>
     );
 };
 export default Search;
