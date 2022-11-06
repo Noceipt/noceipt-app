cd209581210583ca95a8587e4aab02db803c4c75
Christopher Jones <tkcwebdev@gmail.com>
2022-11-06 11:24:59 -0800

    57: Sidenav content added

--- components/Sidenav/index.tsx
+++ components/Sidenav/index.tsx
@@ -1,18 +1,125 @@
-import { Box, IconButton, useColorMode } from "@chakra-ui/react";
-import { FiMoon, FiSun } from "react-icons/fi";
+import {
+	Box,
+	Button,
+	IconButton,
+	Text,
+	useColorMode,
+	useColorModeValue,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { FiMoon, FiSun, FiSettings, FiBell, FiFileText } from "react-icons/fi";
+import avatar from "../../assets/avatar.png";
+import nLogo from "../../assets/noceipt_black.svg";
+import nLogoWhite from "../../assets/noceipt.svg";
+import { useUser } from "../../providers/UserProvider";
 import styles from "./Sidenav.module.scss";
 
 const Sidenav = () => {
+	const navigate = useRouter();
+	const user = useUser();
 	const { colorMode, toggleColorMode } = useColorMode();
-	return (
-		<Box as="aside" bgColor="green" className={styles.Sidenav}>
-			<IconButton aria-label="Toggle Mode" onClick={toggleColorMode}>
-				{colorMode === "light" ? (
+	const linkButtonBG = useColorModeValue("#C0EEA4", "#7AA361");
+	const sideNavLogo = useColorModeValue(nLogo, nLogoWhite);
+	const userLinks = [
+		{
+			label: "General",
+			icon: <FiSettings />,
+			path: "/dashboard/settings",
+		},
+		{
+			label: "Notifications",
+			icon: <FiBell />,
+			path: "/dashboard/settings/notifications",
+		},
+		{
+			label: "Switch Theme",
+			icon:
+				colorMode === "light" ? (
 					<FiMoon size={20} />
 				) : (
 					<FiSun size={20} />
-				)}
-			</IconButton>
+				),
+			onClick: toggleColorMode,
+		},
+		{
+			label: "Privacy Policy",
+			icon: <FiFileText />,
+			path: "/dashboard/settings/privacy",
+		},
+	];
+	return (
+		<Box as="aside" bgColor="green" className={styles.Sidenav}>
+			<Box
+				width="100%"
+				display="flex"
+				justifyContent="center"
+				marginTop={4}
+				textAlign="center"
+				flexDirection="column"
+				alignItems="center"
+			>
+				<Image
+					className={styles.Sidenav__avatar}
+					width={100}
+					height={100}
+					src={avatar}
+					alt="User Profile avatar"
+				/>
+				<Box
+					padding={4}
+					width="100%"
+					bgColor={linkButtonBG}
+					borderRadius={8}
+				>
+					<Text size="md" marginBottom={4}>
+						{user.email}
+					</Text>
+					{userLinks.map((link, index) => {
+						return (
+							<Box
+								marginBottom="2"
+								borderBottom="solid 1px"
+								className={styles.Sidenav__link}
+								key={index}
+							>
+								<Button
+									paddingX={0}
+									className={styles.Sidenav__link}
+									borderRadius={0}
+									backgroundColor="transparent"
+									width="100%"
+									display="flex"
+									justifyContent="space-between"
+									alignItems="center"
+									onClick={() =>
+										link.onClick
+											? link.onClick()
+											: navigate.push(link.path)
+									}
+									_hover={{
+										backgroundColor:
+											colorMode === "light"
+												? "black.100"
+												: "white.100",
+									}}
+									_focus={{
+										backgroundColor: "transparent",
+									}}
+								>
+									{link.icon}
+									<Text>{link.label}</Text>
+								</Button>
+							</Box>
+						);
+					})}
+				</Box>
+				<Image width={50} src={sideNavLogo} alt="Noceipt Logo" />
+				<Text fontSize="sm" marginTop="4">
+					Noceipt Version 0.1.
+					<br /> Copyright 2022. All Rights Reserved
+				</Text>
+			</Box>
 		</Box>
 	);
 };
