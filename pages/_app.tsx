889465734e4bc26f4f0d7cd22b319e6030dcf20d
Christopher Jones <tkcwebdev@gmail.com>
2022-11-05 19:18:44 -0700

    43: Used side nav provider and add page template to home screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "../styles/globals.scss";
 import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../utils/themes";
+import SidenavProvider from "../providers/SidenavProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider theme={theme}>
+			<SidenavProvider>
+				<Component {...pageProps} />
+			</SidenavProvider>
+		</ChakraProvider>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,40 @@
 import { Box, Heading, useColorMode } from "@chakra-ui/react";
-import { IconButton } from '@chakra-ui/button'
+import { IconButton } from "@chakra-ui/button";
 import Head from "next/head";
+import Page from "../templates/Page";
 
 export default function Home() {
+	const { colorMode, toggleColorMode } = useColorMode();
 
-  const { colorMode, toggleColorMode } = useColorMode()
+	return (
+		<>
+			<Head>
+				<title>Noceipt</title>
+				<meta
+					name="description"
+					content="NEVER PRINT A RECEIPT AGAIN"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-  return (
-    <div>
-      <Head>
-        <title>Noceipt</title>
-        <meta name="description" content="NEVER PRINT A RECEIPT AGAIN" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+			<Page>
+				<Box display="flex" height={"50vh"} flexDirection={"column"}>
+					<Heading>{"Noceipt"}</Heading>
+					<IconButton
+						mt={4}
+						aria-label="Toggle Mode"
+						onClick={toggleColorMode}
+					>
+						{colorMode === "light" ? (
+							<h1>{"Dark Mode"}</h1>
+						) : (
+							<h5>{"Light Mode"}</h5>
+						)}
+					</IconButton>
+				</Box>
+			</Page>
 
-      <main>
-      <Box display="flex" height={'50vh'} flexDirection={'column'}>
-        <Heading>{'Noceipt'}</Heading>
-        <IconButton mt={4} aria-label="Toggle Mode" onClick={toggleColorMode}>
-          { colorMode === 'light' ? <h1>{'Dark Mode'}</h1> : <h5>{'Light Mode'}</h5> }
-        </IconButton>
-      </Box>
-        
-        
-      </main>
-
-      <footer>{'Footer'}</footer>
-    </div>
-  );
+			<footer>{"Footer"}</footer>
+		</>
+	);
 }
