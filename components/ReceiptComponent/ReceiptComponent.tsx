e5c792244e445421868cd6f6193173842bfbdc32
Cody Molisee <codymolisee@Codys-MacBook-Pro.local>
2022-11-06 16:06:33 -0800

    shet

--- components/ReceiptComponent/ReceiptComponent.tsx
+++ components/ReceiptComponent/ReceiptComponent.tsx
@@ -9,6 +9,55 @@ interface IReceiptComponentProps {
   receipt: IReceipt;
 }
 
+const fakeAssShit: IReceipt = {
+  company: {
+    name: 'McDonalds',
+    logo: 'img/path',
+    address: '123 McDonalds St',
+    website: 'McDonalds.com',
+  },
+  orderTime: new Date().toString(),
+  paymentMethod: {
+    cardNumber: '1234',
+    cardType: 'MASTERCARD',
+  },
+  items: [
+    {
+      numOfItems: 4,
+      name: 'cookies',
+      value: 55.99,
+      _id: '1'
+    },
+    {
+      numOfItems: 18,
+      name: 'gummy bears',
+      value: 11.11,
+      _id: '2'
+    },
+    {
+      numOfItems: 1,
+      name: 'stove pipe',
+      value: 99.99,
+      _id: '3'
+    },
+    {
+      numOfItems: 100000,
+      name: 'bananas',
+      value: 1.99,
+      _id: '4'
+    }
+  ],
+  subTotal: 1000000.99,
+  tax: 9999.99,
+  total: 1010000.98,
+  memos: [
+    'Return Policy',
+    'thanks for choosing us',
+    'you spent too much money'
+  ],
+  _id: "yes"
+};
+
 const ReceiptComponent = ({
   classname = "",
   receipt,
